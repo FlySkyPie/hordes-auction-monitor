140dfb4abf55fbd6f9d319c21f5094d55cc1992e
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-09-06 16:01:36 +0800

    add auto clicker.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ fetch = (input, init) => {
     return originalFetch(input, init).then(response => {
         // it is not important to create new Promise in ctor, we can await existing one, then wrap result into new one
         return new Promise((resolve) => {
-            if( input === '/api/item/get' &&JSON.parse(init.body).hasOwnProperty('auction')){
-                response.clone().json().then(json=>{
-                    console.log(JSON.stringify(json));
+            if (input === '/api/item/get' && JSON.parse(init.body).hasOwnProperty('auction')) {
+                response.clone().json().then(json => {
+                    //console.log(JSON.stringify(json));
                 })
-                console.log('Hook the fetch!');
+                //console.log('Hook the fetch!');
             }
             resolve(response);
         });
@@ -24,11 +24,24 @@ fetch = (input, init) => {
         button.innerHTML = "W";
         elixirIcon.parentNode.appendChild(button);
         button.addEventListener('click', () => {
+            let isCanvasHide = false;
             // hidden/visible canvases
             let canvases = document.querySelectorAll('.l-canvas');
             canvases.forEach((canvas) => {
+                isCanvasHide = (canvas.style.visibility !== 'hidden');
                 canvas.style.visibility = (canvas.style.visibility === 'hidden') ? 'visible' : 'hidden';
             });
+            //merchant
+            if (isCanvasHide) {
+                let children =
+                    document.querySelector('.js-merchant-initd .choices').childNodes;
+                //document.querySelector('.js-merchant-initd > .slot > .layout > .choices').childNodes;
+                for (let i = 0; i < children.length; i++) {
+                    setTimeout(function () {
+                        children[i].click();
+                    }, 5000 * i);
+                }
+            }
         });
         isAdded = true;
     }
