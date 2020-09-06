d0fdd2643a194c93522e0d4d8ad1270cc4214dce
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-09-06 15:26:46 +0800

    add nav button injecter.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
- 
+!function () {
+    let isAdded = false;
+    function createNavButton() {
+        let elixirIcon = document.querySelector('#sysgem');
+        let button = document.createElement("div");
+        button.className = "btn border black js-marketwatcher-icon";
+        button.innerHTML = "W";
+        elixirIcon.parentNode.appendChild(button);
+        button.addEventListener('click', () => {
+            // hidden/visible canvases
+            let canvases = document.querySelectorAll('.l-canvas');
+            canvases.forEach((canvas) => {
+                canvas.style.visibility = (canvas.style.visibility === 'hidden') ? 'visible' : 'hidden';
+            });
+        });
+        isAdded = true;
+    }
+
+    const pageObserver = new MutationObserver(() => {
+        const isUiLoaded = !!document.querySelector('.layout');
+
+        if (isUiLoaded && !isAdded) {
+            createNavButton();
+        }
+    });
+
+    pageObserver.observe(document.body, { attributes: true, childList: true });
+}();
